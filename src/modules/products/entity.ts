dfd98ec335de90ecdae973e84dd77de172549a0e
Woipot <frostik0409@gmail.com>
2021-06-15 21:12:33 +0300

    add entities

--- src/modules/products/entity.ts
+++ src/modules/products/entity.ts
@@ -1,28 +1,110 @@
 import {
-	Entity,
-	PrimaryGeneratedColumn,
-	Column,
-	CreateDateColumn,
-	UpdateDateColumn
-} from 'typeorm';
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  ManyToOne,
+} from "typeorm";
 
 @Entity()
 export class Product {
-  @PrimaryGeneratedColumn('uuid')
-  _id: string
+  @PrimaryGeneratedColumn("uuid")
+  _id: string;
 
   @Column()
-  name: string
+  name: string;
 
   @Column()
-  unit: number
+  unit: number;
 
   @Column()
-  category: string
+  category: string;
 
   @CreateDateColumn()
-  created_at: string
+  created_at: string;
 
   @UpdateDateColumn()
-  updated_at: string
+  updated_at: string;
+}
+
+@Entity()
+export class Group {
+  @PrimaryGeneratedColumn("uuid")
+  _id: string;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Student, (student) => student.group)
+  students: Student[];
+}
+
+
+@Entity()
+export class Student {
+  @PrimaryGeneratedColumn("uuid")
+  _id: string;
+
+  @Column()
+  full_name: string;
+
+  @OneToMany(() => Task, (task) => task.student)
+  tasks: Task[];
+
+  @ManyToOne(() => Group, (group) => group.students)
+  group: Group;
+}
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn("uuid")
+  _id: string;
+
+  @Column()
+  comment: string;
+
+  @Column()
+  status: string;
+
+  @CreateDateColumn()
+  task_file: string;
+
+  @OneToMany(() => TaskAttempt, (attempt) => attempt.task)
+  attempts: TaskAttempt[];
+
+  @ManyToOne(() => Student, (student) => student.tasks)
+  student: Student;
+}
+
+
+@Entity()
+export class TaskAttempt {
+  @PrimaryGeneratedColumn("uuid")
+  _id: string;
+
+  @Column()
+  task_id: string;
+
+  @Column()
+  student_file: string;
+
+  @CreateDateColumn()
+  teachter_file: string;
+
+  @Column()
+  max_mark: number;
+
+  @Column()
+  mark: number;
+
+  @Column()
+  status: string;
+
+  @CreateDateColumn()
+  created_at: string;
+
+  @ManyToOne(() => Task, (task) => task.attempts)
+  task: Task;
 }
--- src/plugins/db.ts
+++ src/plugins/db.ts
@@ -5,7 +5,7 @@ import {
 	createConnection,
 	getConnectionOptions,
 } from 'typeorm';
-import { Product } from '../modules/products/entity';
+import { Group, Product, Student, Task, TaskAttempt } from '../modules/products/entity';
 
 import * as path from 'path';
 
@@ -15,22 +15,26 @@ export default fp(async (server) => {
 	try {
 		const connectionOptions = await getConnectionOptions();
 		Object.assign(connectionOptions, {
-			options: { encrypt: true },
-			entities: [Product],
+			options: { encrypt: false },
+			entities: [Product, Group, Student, Task, TaskAttempt],
 		});
 
 		const options: ConnectionOptions = {
 			type: 'sqlite',
 			database: `${root}/data/db.sqlite`,
-			entities: [Product],
+			entities: [Product, Group, Student, Task, TaskAttempt],
 			logging: true,
 		};
 
-		const connection = await createConnection(options)
+		const connection = await createConnection(options);
 		console.log('database connected');
 
 		server.decorate('db', {
 			products: connection.getRepository(Product),
+			attempts: connection.getRepository(TaskAttempt),
+			tasks: connection.getRepository(Task),
+			students: connection.getRepository(Student),
+			groups: connection.getRepository(Group),
 		});
 	} catch (error) {
 		console.log(error);
