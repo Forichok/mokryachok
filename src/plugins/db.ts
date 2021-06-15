e2624108b66b4843c9e5c95e1a7871d0484eb2af
Forichok <nnudalov@gmail.com>
2021-06-15 20:05:17 +0300

    add best database ever

--- src/plugins/db.ts
+++ src/plugins/db.ts
@@ -1,21 +1,36 @@
 import 'reflect-metadata';
 import fp from 'fastify-plugin';
-import { createConnection, getConnectionOptions } from 'typeorm';
+import {
+	ConnectionOptions,
+	createConnection,
+	getConnectionOptions,
+} from 'typeorm';
 import { Product } from '../modules/products/entity';
 
-export default fp(async server => {
+import * as path from 'path';
+
+const root: string = path.resolve(__dirname, '..');
+
+export default fp(async (server) => {
 	try {
 		const connectionOptions = await getConnectionOptions();
 		Object.assign(connectionOptions, {
 			options: { encrypt: true },
-			entities: [Product]
+			entities: [Product],
 		});
 
-		const connection = await createConnection(connectionOptions);
+		const options: ConnectionOptions = {
+			type: 'sqlite',
+			database: `${root}/data/db.sqlite`,
+			entities: [Product],
+			logging: true,
+		};
+
+		const connection = await createConnection(options)
 		console.log('database connected');
 
 		server.decorate('db', {
-			products: connection.getRepository(Product)
+			products: connection.getRepository(Product),
 		});
 	} catch (error) {
 		console.log(error);
